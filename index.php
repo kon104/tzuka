18906a3fe42444ab91e3cff9c374f6292f0c2898
kon104 <toshiharu@MacBook-2017.local>
2018-01-06 03:14:18 +0900

    added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php>
+
+?>
+<html>
+<head>
+</head>
+<body>
+<h2>Ｔ塚市向けデモサイト</h2>
+<hr/>
+
+<ul>
+	<li><a href="./kml-sample-app/yolp.php">KMLのポリゴンで自治体を表現 for yolp</a></li>
+	<li><a href="./kml-sample-app/gmap.php">実験 for gmap</a></li>
+	<li><a href="./www/">Home Page 改善案</a></li>
+</ul>
+
+
+</body>
+</html>
