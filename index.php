7b9ef87e193f97d8edc9bf5e12e7b3bd51661ce1
kon104 <konuma10486@yahoo.co.jp>
2018-01-08 16:28:39 +0900

    changed location to put app's files

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <hr/>
 
 <ul>
-	<li><a href="./kml-sample-app/yolp.php">KMLのポリゴンで自治体を表現 for yolp</a></li>
-	<li><a href="./kml-sample-app/gmap.php">実験 for gmap</a></li>
-	<li><a href="./open-data/govt-services.php">年代別の行政サービス</a></li>
+	<li><a href="./welfare-dept-app/yolp.php">KMLのポリゴンで自治体を表現 for yolp</a></li>
+	<li><a href="./welfare-dept-app/gmap.php">実験 for gmap</a></li>
+	<li><a href="./welfare-dept-app/govt-services.php">年代別の行政サービス</a></li>
 	<li><a href="./www/">Home Page 改善案</a></li>
 </ul>
 
