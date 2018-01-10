a267bbccc0b381a112e2ba3dc76c933b699bf551
kon104 <konuma10486@yahoo.co.jp>
2018-01-10 23:48:21 +0900

    improved to short paths.

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 ?>
 <html>
 <head>
+<meta http-equiv="Content-Type" Content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <h2>Ｔ塚市向けデモサイト</h2>
 <hr/>
 
 <ul>
-	<li><a href="./welfare-dept-app/yolp.php">KMLのポリゴンで自治体を表現 for yolp</a></li>
-	<li><a href="./welfare-dept-app/gmap.php">実験 for gmap</a></li>
-	<li><a href="./welfare-dept-app/govt-services.php">年代別の行政サービス</a></li>
+	<li><a href="./welfare/yolp.php">KMLのポリゴンで自治体を表現 for yolp</a></li>
+	<li><a href="./welfare/gmap.php">実験 for gmap</a></li>
+	<li><a href="./welfare/govt-services.php">年代別の行政サービス</a></li>
 	<li><a href="./www/">Home Page 改善案</a></li>
 </ul>
 
